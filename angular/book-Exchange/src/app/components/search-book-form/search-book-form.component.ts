fc42205d7ede3c0d238597bb12b8ef9c86db78f7
tasosApostolou <thoukis95@gmail.com>
2024-06-10 10:02:37 +0300

    dfa

--- angular/book-Exchange/src/app/components/search-book-form/search-book-form.component.ts
+++ angular/book-Exchange/src/app/components/search-book-form/search-book-form.component.ts
@@ -2,22 +2,13 @@ import { Component, inject,QueryList, ViewChildren, ElementRef, ViewChild, Event
 import { FormControl, FormGroup, NgForm, NgModel, NgModelGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Title } from '@angular/platform-browser';
-import { Book, BookWithPersons, BookWithUsers } from 'src/app/shared/interfaces/book';
-import { User } from 'src/app/shared/interfaces/user';
-import { BookService } from 'src/app/shared/services/book.service';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
-
-import { UserService } from 'src/app/shared/services/user.service';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { UserLoginComponent } from '../user-login/user-login.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenu } from '@angular/material/menu';
-import { MatToolbar, MatToolbarModule, MatToolbarRow } from '@angular/material/toolbar';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import { NotificationService } from 'src/app/shared/services/notification.service';
-import { Person } from 'src/app/shared/interfaces/person';
 import { BooksInStoresComponent } from '../books-in-stores/books-in-stores.component';
 import { SearchBookForm } from 'src/app/shared/interfaces/Search-book-form'; 
 
