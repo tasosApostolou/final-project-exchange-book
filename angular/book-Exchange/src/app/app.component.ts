079d924daf2d2e745a5526bf2a888f89ac61b17d
tasosApostolou <thoukis95@gmail.com>
2024-06-14 13:07:14 +0300

    dada

--- angular/book-Exchange/src/app/app.component.ts
+++ angular/book-Exchange/src/app/app.component.ts
@@ -5,24 +5,10 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar-component/navbar.component';
 
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { MatIcon } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-
-// import { NavbarComponent } from './components/navbar/navbar.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet,NavbarComponent,WelcomeComponent,MatIcon,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
+  imports: [RouterLink, RouterOutlet,NavbarComponent,WelcomeComponent
     // BrowserAnimationsModule
   ],
   templateUrl: './app.component.html',
